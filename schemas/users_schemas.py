ae5bf8fcd9ed619808e83831924b869acf137992
Gabriel Drumond <gabriel_s4@hotmail.com>
2026-03-20 08:51:35 -0300

    feat(backend):Limpeza de entidades para realidade do projeto, inclusão de novo pipeline para ajuste de features.

--- schemas/users_schemas.py
+++ schemas/users_schemas.py
@@ -8,46 +8,25 @@ class users(SC_BaseModel):
     id:Optional[int] = None
     name: str
     email:EmailStr
-    cpf: str
-    phone: str
     created_at:Optional[datetime] = datetime.now()
     active:Optional[bool] = True
     role_id: Optional[int] = 1
-    reset_password_token:Optional[str] = None
-    reset_password_expires:Optional[datetime] = None
     class Config:
         from_attributes = True
-
 
 class users_update(users):
     name:Optional[str] = None
     email:Optional[EmailStr] = None
-    cpf: Optional[str] = None
     password:Optional[str] = None
-    phone:Optional[str] = None
     active:Optional[bool] = True
     role_id:Optional[int] = None
-    reset_password_token:Optional[str] = None
-    reset_password_expires:Optional[datetime] = None
 
 class users_create(users):
     password:str
-    phone:str
 
 class usersGetData(users):
     name:Optional[str] = None
     email:Optional[EmailStr] = None
-    cpf: Optional[str] = None
-    phone:Optional[str] = None
     active:Optional[bool] = True
     role:Optional[str] = None
-    tarefas:Optional[int] = None
 
-class users_updateForm(SC_BaseModel):
-    name:Optional[str] = None
-    email:Optional[EmailStr] = None
-    cpf: Optional[str] = None
-    phone:Optional[str] = None
-    active:Optional[bool] = True
-    class Config:
-        from_attributes = True
--- system_models/users.py
+++ system_models/users.py
@@ -8,19 +8,11 @@ class Users(modelsGeneric):
     password = Column(String(255),nullable=False)
     name = Column(String(50),nullable=False)
     email = Column(String(50),nullable=False)
-    phone = Column(String(12),nullable=False)
-    cpf = Column(String(12), unique=True,nullable=False)
     role_id = Column(Integer,ForeignKey('roles.id'),default=1)
-    reset_password_token = Column(String(255),nullable=True)
-    reset_password_expires = Column(DateTime,nullable=True)
- 
     
     #relação da FK pra apontar o relacionamento de role para usuario
     role = relationship("Roles",lazy='joined',back_populates='user')
-    
-    # Relação reversa para rotinas
-    routine = relationship("Routines", back_populates="user")
-    user_clients = relationship("Users_Clients", back_populates="user")
+
     
     
  
